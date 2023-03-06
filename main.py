d70db835e7c297d6bfac91e0562e05cc44dbbdd1
Ori <orimoshe02@gmail.com>
2023-03-06 20:42:41 +0200

    messy but calculation 2 works

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 def get_user_inputs():
     global NUMBER_OF_WATS_IN_CHECK, NUMBER_OF_CHECKS, NUMBER_OF_WATS_IN_CHECK, WATS_OF_SMALL_APARTMENT_NOW \
-        , WATS_OF_BOTH_APARTMENTS
+        , WATS_OF_BOTH_APARTMENTS, LAST_WATS_OF_SMALL_APARTMENT
     NUMBER_OF_CHECKS = int(input("enter number of checks: "))
 
     NUMBER_OF_WATS_IN_CHECK = [0 for check in range(NUMBER_OF_CHECKS)] 
@@ -12,11 +12,9 @@ def get_user_inputs():
         NUMBER_OF_WATS_IN_CHECK[check] = int(input("enter number of wats in check: "))
         NUMBER_OF_PRICES_IN_CHECK[check] = int(input("enter prices of wats in check: "))
     
+    LAST_WATS_OF_SMALL_APARTMENT = int(input("enter last wats of small apartment: "))
     WATS_OF_SMALL_APARTMENT_NOW = int(input("enter wats of small apartment now: "))
     WATS_OF_BOTH_APARTMENTS = int(input("enter wats of both apartment: "))
-    
-    last_wats_of_small_apartment = int(input("enter last wats of small apartment: "))
-    return last_wats_of_small_apartment
     
 def calculate_precentage_of_wats_in_check():
     precentage_of_wats_in_check = [0 for check in range(NUMBER_OF_CHECKS)]
@@ -26,10 +24,15 @@ def calculate_precentage_of_wats_in_check():
 
     return precentage_of_wats_in_check
 
+def calculate_wats_of_small_apartment_in_all_checks():
+    return (WATS_OF_SMALL_APARTMENT_NOW-LAST_WATS_OF_SMALL_APARTMENT)
+    
 def main():
-    last_wats_of_small_apartment = get_user_inputs()
+    get_user_inputs()
 
     precentage_of_wats_in_check = calculate_precentage_of_wats_in_check()
-    
+    wats_of_small_apartment_in_all_checks = calculate_wats_of_small_apartment_in_all_checks()    
+
+
 if __name__ == "__main__":
     main()
