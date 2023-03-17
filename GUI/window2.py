5e9202ed7101a648a1b68b08b0cb75a909634593
Ori <orimoshe02@gmail.com>
2023-03-18 01:41:22 +0200

    in process of adding all variables to GUI, messy

--- GUI/window2.py
+++ GUI/window2.py
@@ -4,14 +4,9 @@ from functools import partial
 
 class window2_manager():
     def __init__(self, number_of_checks, parent=None):
-        super().__init__()
-        
-        print("init2")
-        
+        super().__init__()        
         self.number_of_checks = int(number_of_checks.text())
         self.create_complete_input_window()
-        
-        print("end init2")
     
     def create_complete_input_window(self):
         self.create_widget()
@@ -27,7 +22,7 @@ class window2_manager():
         self.sumbitButton2.clicked.connect(partial(self.button_click2, self))
 
     def create_layout(self):
-        self.number_of_wats_in_check = []
+        # self.number_of_wats_in_check = []
         self.layout = QFormLayout()
         
         self.add_rows_to_layout()
@@ -35,17 +30,26 @@ class window2_manager():
         self.window2.setLayout(self.layout)
 
     def add_rows_to_layout(self):
-        self.add_number_of_wats_row()
+        info_parameters = ["number_of_wats_in_check", "wats_of_small_apartment_now"]
+        self.info = {}
+            
+        for parameter in info_parameters:
+            for check in range(self.number_of_checks):
+                item_name = parameter + "_" + str(check)
+                
+                self.info[item_name] = QLineEdit()            
+                self.info[item_name].setValidator(QDoubleValidator())
+
+
+        for item_name, item_value in self.info.items():
+            self.add_number_of_wats_row(item_name, item_value)
 
         self.layout.addRow("Button2", self.sumbitButton2)
         
-    def add_number_of_wats_row(self):
-        for check in range(self.number_of_checks):
-            self.number_of_wats_in_check.append(QLineEdit())
-            self.number_of_wats_in_check[check].setValidator(QDoubleValidator())
-                
-            self.layout.addRow("number_of_wats_in_check " + str(check), self.number_of_wats_in_check[check])        
+    def add_number_of_wats_row(self, item_name, item_value):
+            self.layout.addRow(item_name, item_value) 
         
     def button_click2(self, a, b):
-        for i in range(0, len(self.number_of_wats_in_check)):
-            print(self.number_of_wats_in_check[i].text())
+        input_values = self.info.values()
+        for value in input_values:
+            print(value.text())
