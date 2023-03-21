54ea87af2534b0c0554a3ccef30eef46d1eae909
Ori <orimoshe02@gmail.com>
2023-03-21 02:05:55 +0200

    added \n to output

--- Utils/prints.py
+++ Utils/prints.py
@@ -1,22 +1,21 @@
 import Utils.global_variables as g
-from PyQt5.QtGui import QDoubleValidator, QFont
 
 def base_data(self):
     word = " חשבון החשמל לתקופה שבין"
     word2 = " עד "
-    sentence = word + " " + str(g.START_DATE) + word2 + str(g.END_DATE) + ":\n"
+    sentence = word + " " + str(g.START_DATE) + word2 + str(g.END_DATE) + ":\n\n"
         
     word = "בחשבון זה צריכה כוללת "
-    sentence += word + str(g.WATS_OF_BOTH_APARTMENTS) + " קוטש" + "\n"
+    sentence += word + str(g.WATS_OF_BOTH_APARTMENTS) + " קוטש" + "\n\n"
     
     word = "קריאה עדכנית של היחידה "
     sentence += word + str(g.WATS_OF_SMALL_APARTMENT_NOW) + "\n"
     
     word = "קריאה קודמת של היחידה "
-    sentence += word + str(g.LAST_WATS_OF_SMALL_APARTMENT) + "\n"
+    sentence += word + str(g.LAST_WATS_OF_SMALL_APARTMENT) + "\n\n"
     
     word = "צריכה של היחידה בתקופה זו "
-    sentence += word + str(round(g.WATS_USAGE_OF_SMALL_APARTMENT, 1)) + " קוטש" + "\n"
+    sentence += word + str(round(g.WATS_USAGE_OF_SMALL_APARTMENT, 1)) + " קוטש" + "\n\n"
 
     word = "בחשבון זה חברת חשמל חייבה ב "
     word2 = " תעריפים כדלהלן:"
@@ -29,7 +28,7 @@ def base_data(self):
         sentence += str(check+1) + "." + word + str(g.CHANGE_PRICE_DATE[check]) + " " + str(round(g.PRICES_OF_WATS_IN_CHECK[check]* 100, 2)) \
             + word2 + str(g.NUMBER_OF_WATS_IN_CHECK[check]) + word3 + str(round(g.PRECENTAGE_OF_WATS_IN_CHECK[check] * 100)) + "%" + "\n"
     
-    word = "חישוב תשלום היחידה"
+    word = "\n" + "חישוב תשלום היחידה"
     sentence += word + "\n"
     for check in range(g.NUMBER_OF_CHECKS):
         sentence += str(check+1) + ". " + str(round(g.PRECENTAGE_OF_WATS_IN_CHECK[check] * 100)) + "% " + "מתוך " + \
@@ -37,7 +36,7 @@ def base_data(self):
         " קוטש " + "כפול " + str(round(g.PRICES_OF_WATS_IN_CHECK[check]* 100, 2)) + " אגורות " + " = " + \
         str(round(g.COST_OF_WATS_IN_CHECK[check], 2)) + " שח ללא מעמ" + "\n"
 
-    word = "הוספת מעמ"
+    word = "\n" + "הוספת מעמ"
     sentence += word + "\n"
     total_cost_in_check = 0.0
     for check in range(g.NUMBER_OF_CHECKS):
@@ -62,4 +61,3 @@ def print_on_GUI(self, word):
 
     self.widget.setWindowTitle("Output")
     self.widget.show()
-        
